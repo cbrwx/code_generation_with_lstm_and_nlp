8715fe68d06db3dd2641e16cc6c4c301d4fcdf77
cbrwx <81207429+cbrwx@users.noreply.github.com>
2023-02-26 11:21:46 +0100

    Create main_definitions.py

---
+++ main_definitions.py
@@ -0,0 +1,192 @@
+# To enable the file to function properly, it is necessary to remove its extension and rename it... cbrwx
+
+import mysql.connector
+from typing import List, Tuple
+import random
+from mysql.connector import errorcode
+from bayes_opt import BayesianOptimization
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+from keras import optimizers
+from main_definitions import prepare_data
+import spacey
+
+def optimize_neural_network(X_train, y_train, X_val, y_val, input_dim, output_dim):
+
+    def lstm_optimizer(dropout_rate, learning_rate, lstm_units):
+        model = Sequential()
+        model.add(LSTM(lstm_units, input_shape=(input_dim, 1)))
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(output_dim, activation='softmax'))
+        adam = optimizers.Adam(lr=learning_rate)
+        model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+        model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=1, batch_size=32)
+        score, acc = model.evaluate(X_val, y_val, verbose=0)
+        return acc
+
+    bo = BayesianOptimization(lstm_optimizer, {'dropout_rate': (0.1, 0.5), 'learning_rate': (0.0001, 0.01), 'lstm_units': (32, 256)})
+    bo.maximize(n_iter=5, acq='ucb', kappa=2.576)
+    best_params = bo.max['params']
+    return best_params
+
+# Use a file I/O operation to write the generated code examples to a file
+def write_generated_code_to_file(generated_code, file_path):
+    with open(file_path, 'w') as f:
+        f.write(generated_code)
+        
+def get_test_cases():
+    return [(['input1', 'input2'], 'expected_output1'),
+            (['input3', 'input4'], 'expected_output2'),
+            (['input5', 'input6'], 'expected_output3')]
+
+# Assign a numeric value as a reward to the program based on the generated code's functionality
+def assign_reward(generated_code, test_cases):
+    score = evaluate_generated_code(generated_code, test_cases)
+    if score < 0:
+        score = 0
+    reward = 0.0
+    if score == 1.0:
+        reward = 1.0
+    elif score >= 0.8:
+        reward = 0.8
+    elif score >= 0.6:
+        reward = 0.6
+    elif score >= 0.4:
+        reward = 0.4
+    elif score >= 0.2:
+        reward = 0.2
+    return reward        
+                
+def evaluate_generated_code(generated_code, test_cases):
+    num_passed = 0
+    for test_case in test_cases:
+        try:
+            inputs = test_case[0]
+            expected_output = test_case[1]
+            exec(generated_code)
+            output = main(*inputs)
+            if output == expected_output:
+                num_passed += 1
+        except:
+            pass
+    score = num_passed / len(test_cases)
+    return score
+
+def connect_to_database():
+    # Connect to MySQL database
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="database_name"
+    )
+    print("Connected to database")
+    return db
+
+def get_code_examples(database, user, password):
+    try:
+        # Connect to the database
+        cnx = mysql.connector.connect(user=user, password=password,
+                                      host='localhost',
+                                      database=database)
+        cursor = cnx.cursor()
+
+        # Retrieve code examples from the database
+        query = "SELECT code, is_correct FROM code_examples"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        # Separate correct and incorrect code examples
+        correct_examples = []
+        incorrect_examples = []
+        for row in rows:
+            if row[1] == 1:
+                correct_examples.append(row[0])
+            else:
+                incorrect_examples.append(row[0])
+
+        return correct_examples, incorrect_examples
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+
+        return [], []
+
+def prepare_data_for_lstm(database, user, password):
+    # Retrieve code examples from the database
+    db = connect_to_database(database, user, password)
+    cursor = db.cursor()
+    correct_examples, incorrect_examples = get_code_examples(cursor)
+
+    # Modify code examples
+    modified_correct_examples, modified_incorrect_examples = modify_code_examples(correct_examples, incorrect_examples)
+
+    # Tokenize code examples
+    tokenized_correct_examples = []
+    tokenized_incorrect_examples = []
+    for example in modified_correct_examples:
+        tokenized_correct_examples.append(tokenize_code(example))
+    for example in modified_incorrect_examples:
+        tokenized_incorrect_examples.append(tokenize_code(example))
+
+    # Use pairs of input-output sequences, where the input sequence is a partial code snippet and the output sequence is the next token in the code       
+    data = []
+    labels = []
+    for example in tokenized_correct_examples:
+        for i in range(len(example)-1):
+            data.append(example[:i+1])
+            labels.append(example[i+1])
+    for example in tokenized_incorrect_examples:
+        for i in range(len(example)-1):
+            data.append(example[:i+1])
+            labels.append(example[i+1])
+
+    # Convert data and labels to numerical format
+    token_to_num = dict(zip(set(labels), range(len(set(labels)))))
+    num_to_token = dict(zip(range(len(set(labels))), set(labels)))
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            data[i][j] = token_to_num[data[i][j]]
+        labels[i] = token_to_num[labels[i]]
+
+    return data, labels, token_to_num, num_to_token
+    
+def modify_code_examples(correct_examples: List[str], incorrect_examples: List[str]) -> Tuple[List[str], List[str]]:
+    modified_correct_examples = []
+    modified_incorrect_examples = []
+
+    # Apply mutation or crossover techniques to the code examples
+    for i in range(len(correct_examples)):
+        # Randomly select whether to mutate or cross over
+        if random.random() < 0.5:
+            # Mutate the code by changing a random character
+            mutated_code = list(correct_examples[i])
+            index = random.randint(0, len(mutated_code) - 1)
+            mutated_code[index] = chr(ord(mutated_code[index]) + random.randint(-1, 1))
+            modified_correct_examples.append("".join(mutated_code))
+        else:
+            # Cross over with another correct code example
+            partner_index = random.randint(0, len(correct_examples) - 1)
+            crossover_index = random.randint(0, len(correct_examples[i]) - 1)
+            modified_correct_examples.append(correct_examples[i][:crossover_index] + correct_examples[partner_index][crossover_index:])
+
+    for i in range(len(incorrect_examples)):
+        # Randomly select whether to mutate or cross over
+        if random.random() < 0.5:
+            # Mutate the code by changing a random character
+            mutated_code = list(incorrect_examples[i])
+            index = random.randint(0, len(mutated_code) - 1)
+            mutated_code[index] = chr(ord(mutated_code[index]) + random.randint(-1, 1))
+            modified_incorrect_examples.append("".join(mutated_code))
+        else:
+            # Cross over with another incorrect code example
+            partner_index = random.randint(0, len(incorrect_examples) - 1)
+            crossover_index = random.randint(0, len(incorrect_examples[i]) - 1)
+            modified_incorrect_examples.append(incorrect_examples[i][:crossover_index] + incorrect_examples[partner_index][crossover_index:])
+
+    return modified_correct_examples, modified_incorrect_examples
